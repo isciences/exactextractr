29c162e37e9a1b3fb865baf49829146c26b255c2
Dan Baston <dbaston@isciences.com>
2019-07-10 10:43:08 -0400

    Rename partial_mask to coverage_fraction
    This is an attempt to use more clear and consistent terminology.

--- tests/testthat/test_coverage_fraction.R
+++ tests/testthat/test_coverage_fraction.R
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 ISciences, LLC.
+# Copyright (c) 2018-2019 ISciences, LLC.
 # All rights reserved.
 #
 # This software is licensed under the Apache License, Version 2.0 (the "License").
@@ -11,9 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-context('partial_mask')
+context('coverage_fraction')
 
-test_that("Partial mask function works", {
+test_that("Coverage fraction function works", {
   # This test just verifies a successful journey from R
   # to C++ and back. The correctness of the algorithm
   # is tested at the C++ level.
@@ -26,7 +26,7 @@ test_that("Partial mask function works", {
 
   rast <- raster::raster(xmn=0, xmx=3, ymn=0, ymx=3, nrows=3, ncols=3)
 
-  weights <- partial_mask(rast, square)
+  weights <- coverage_fraction(rast, square)
 
   expect_equal(as.matrix(weights[[1]]),
                rbind(
@@ -36,32 +36,30 @@ test_that("Partial mask function works", {
                ), check.attributes=FALSE)
 })
 
-test_that('Raster returned by partial_mask has same properties as the input', {
+test_that('Raster returned by coverage_fraction has same properties as the input', {
   r <- raster::raster(xmn=391030, xmx=419780, ymn=5520000, ymx=5547400)
   raster::res(r) = c(100, 100)
   raster::values(r) <- 1:ncell(r)
 
   p <- sf::st_as_sfc('POLYGON((397199.680921053 5541748.05921053,402813.496710526 5543125.03289474,407103.299342105 5537246.41447368,398470.733552632 5533962.86184211,397199.680921053 5541748.05921053))')
 
-  w <- partial_mask(r, p)
+  w <- coverage_fraction(r, p)
 
   expect_length(w, 1)
   expect_is(w[[1]], 'RasterLayer')
 
   expect_equal(raster::res(r),    raster::res(w[[1]]))
   expect_equal(raster::extent(r), raster::extent(w[[1]]))
-
-
 })
 
-test_that('Coverage percentages are exact', {
+test_that('Coverage fractions are exact', {
   r <- raster::raster(xmn=391030, xmx=419780, ymn=5520000, ymx=5547400)
   raster::res(r) = c(100, 100)
   raster::values(r) <- 1:ncell(r)
 
   p <- sf::st_as_sfc('POLYGON((397199.680921053 5541748.05921053,402813.496710526 5543125.03289474,407103.299342105 5537246.41447368,398470.733552632 5533962.86184211,397199.680921053 5541748.05921053))')
 
-  w <- partial_mask(r, p)
+  w <- coverage_fraction(r, p)
 
   cell_area <- prod(raster::res(w[[1]]))
   ncells <- raster::cellStats(w[[1]], 'sum')
